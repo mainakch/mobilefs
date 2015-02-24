53ae679364713032f5482f3eb9aba89a10045776
Mainak Chowdhury <mainakch@gmail.com>
2015-02-24 14:02:16 -0800

    refactored code with class

--- network_server.py
+++ network_server.py
@@ -9,6 +9,7 @@ import json
 import sys
 import select
 import time
+import logging
 
 log = logging.getLogger('network_server')
 log.setLevel(logging.DEBUG)
@@ -17,191 +18,196 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 log.addHandler(ch)
 
-#socket address
-public_address = ("corn29.stanford.edu", 60002)
+class Networkserver():
+    def __init__(self):
+        #socket address
+        self.public_address = ("localhost", 60002)
 
-#list of sockets
-network_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-#initialize the sockets
-try:
-    network_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-    network_server.bind(public_address)
-
-except Exception as ex:
-    log.debug(ex)
-    
-inputs = [network_server]
-
-outputs = [network_server]
-
-#queues
-
-#key = taskid, value = request
-transmit_queue = {}
-#key = original_taskid, value = response
-receive_queue = {}
-#key = (priority, taskid, original_taskid, chunknum, chunktotalnum, timestamp when added to queue), value = chunkof request
-chunk_queue = {}
-#key = (taskid, original_taskid, chunknum, chunktotalnum, timestamp when received), value=chunkofresponse
-receive_chunk_queue = {}
-#(received taskid, timestamp)
-completed_tasks = {}
-#timestamp of last transmission
-timestamp_transmission = 0
-
-#mapping from socket object to taskid
-order_of_keys_in_chunk_queue = []
-taskid = 0
-packets_in_flight = 0
-
-def execute_message(taskstring):
-    args = json.loads(taskstring)
-    response = None
-    try:
-        if args[0] == 'chmod':
-            os.chmod(args[1], args[2])
-
-        if args[0] == 'close':
-            os.close(fh)
-
-        if args[0] == 'link':
-            os.link(args[1], args[2])
-
-        if args[0] == 'listdir':
-            response = os.listdir(args[1])
-
-        if args[0] == 'lseekread':
-            os.lseek(args[1], args[2], 0)
-            response = os.read(args[1], args[3])
-
-        if args[0] == 'lseekwrite':
-            os.lseek(args[1], args[2], 0)
-            response = os.write(args[1], args[3])
-
-        if args[0] == 'lstat':
-            response = os.lstat(args[1])
-
-        if args[0] == 'mkdir':
-            os.mkdir(args[1], args[2])
-
-        if args[0] == 'mknod':
-            os.mknod(args[1], args[2], args[3])
-
-        if args[0] == 'open':
-            os.open(args[1], args[2])
+        #list of sockets
+        self.network_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #initialize the sockets
+        try:
+            self.network_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.network_server.bind(self.public_address)
             
-        if args[0] == 'readlink':
-            response = os.readlink(args[1])
-
-        if args[0] == 'rename':
-            response = os.rename(args[1], args[2])
-
-        if args[0] == 'rmdir':
-            response = os.rmdir(args[1])
-
-        if args[0] == 'statvfs':
-            response = os.statvfs(args[1])
-
-        if args[0] == 'symlink':
-            response = os.symlink(args[1], args[2])
-
-        if args[0] == 'unlink':
-            response = os.unlink(args[1])
-
-    except OSError as exc:
-        response = exc
-
-    if response is None:
-        return json.dumps(('non', response))
-    if isinstance(response, Exception):
-        return json.dumps(('err', response.errno))
-    else:
-        return json.dumps(('res', response))
-
-def handle_remote_request(s):
-    try:
-        #s is a network client connection
-        data, network_client_address = s.recvfrom(512)
-        obj = json.loads(data)
-        if obj[2] == 'ack':
-            log.debug('ack %s' % repr(obj))
-            #find out key info
-            candidate_list = [ctr for ctr in order_of_keys_in_chunk_queue if ctr[1] == obj[1][0] and ctr[3] == obj[1][2]]
-            #remove from chunk_queue
-            key = candidate_list[0]
-            order_of_keys_in_chunk_queue.remove(key)
-            del chunk_queue[key]
-            packets_in_flight -= 1
-        if obj[2] == 'pac' and obj[0][0] not in completed_tasks:
-            log.debug('pac %s' % repr(obj))
-            #add to receive chunk queue queue
-            key = augment_timestamp_info_key(obj[0])
-            val = obj[1]
-            #add packet to receive chunk
-            receive_chunk_queue[key] = val
-            #send ack
-            s.sendto(json.dumps([0, obj[0], 'ack']), network_client_address)
-            #check if all packets have been received for the same taskid
-            #there's a more efficient way to do this
-            list_of_recv_chunks = [ctr for ctr in receive_chunk_queue.keys() if ctr[0] == key[0]]
-            if len(list_of_recv_chunks) == key[3]:
-                list_of_recv_chunks.sort(key = lambda x: x[2])
-                #all chunks have been received
-                #transfer to receive_queue
-                receive_queue[key[0]] = ''.join([receive_chunk_queue.pop(ctr) for ctr in list_of_recv_chunks])
-                #mark timestamp in completed queue
-                completed_tasks[key[0]] = time.time()
-
-                #execute action
-                string_response = execute_message(receive_queue.pop(key[0]))
-                
-                #now send response
-                taskid += 1
-                #add message to the chunk_queue
-                (keys, chunks) = split_task(taskid, key[0], string_response)
-                #add keys to order_of_keys_in_chunk_queue
-                order_of_keys_in_chunk_queue.extend(keys)
-                #sort by priority
-                order_of_keys_in_chunk_queue.sort(key = lambda x: x[0])
-                #add entries to chunk_queue
-                for (key, val) in zip(keys, chunks):
-                    chunk_queue[key] = val
-    except:
-        pass                    
+        except Exception as ex:
+            log.debug(ex)
     
-def send_remote_response(s):
-    if packets_in_flight < 1 and len(order_of_keys_in_chunk_queue)>0:
-        #send the topmost priority packet if number of packets in flight is <1
-        #introduce some packet timeout mechanisms
-        key = order_of_keys_in_chunk_queue[0]
-        s.sendto(json.dumps([remove_priority_timestamp_info_from_key(key), chunk_queue[key], 'pac']), network_client_address)
-        packets_in_flight += 1
+        self.inputs = [self.network_server]
+        self.outputs = [self.network_server]
 
-    
-def split_task(taskid, original_taskid, taskstring):
-    #this splits up the taskstring into a list of chunks
-    startpt = range(0, len(taskstring), 100)
-    chunks = [taskstring[pt:pt + 100] for pt in startpt[:-1]]
-    chunks.append(taskstring[startpt[-1]:len(taskstring)])
-    #smaller the task higher the priority
-    ids = [(len(taskstring), taskid, original_taskid, ctr, len(chunks), time.time()) for ctr in range(len(chunks))]
-    return (ids, chunks)
+        #queues
+        
+        #key = taskid, value = request
+        self.transmit_queue = {}
+        #key = original_taskid, value = response
+        self.receive_queue = {}
+        #key = (priority, taskid, original_taskid, chunknum, chunktotalnum, timestamp when added to queue), value = chunkof request
+        self.chunk_queue = {}
+        #key = (taskid, original_taskid, chunknum, chunktotalnum, timestamp when received), value=chunkofresponse
+        self.receive_chunk_queue = {}
+        #(received taskid, timestamp)
+        self.completed_tasks = {}
+        #timestamp of last transmission
+        self.timestamp_transmission = 0
 
-def remove_priority_timestamp_info_from_key(key):
-    return (key[1], key[2], key[3], key[4])
+        #mapping from socket object to taskid
+        self.order_of_keys_in_chunk_queue = []
+        self.taskid = 0
+        self.packets_in_flight = 0
 
-def augment_timestamp_info_key(key):
-    return (key[0], key[1], key[2], key[3], time.time())
-    
-while inputs:
-    readable, writable, exceptional = select.select(inputs, outputs, inputs)
-    for s in readable:
-        handle_remote_request(s)
-    for s in writable:
-        send_remote_response(s)
-            
-    for s in exceptional:
-        inputs.remove(s)
-        if s in outputs:
-            outputs.remove(s)
+    def execute_message(self, taskstring):
+        args = json.loads(taskstring)
+        response = None
+        try:
+            if args[0] == 'chmod':
+                os.chmod(args[1], args[2])
+
+            if args[0] == 'close':
+                os.close(fh)
+
+            if args[0] == 'link':
+                os.link(args[1], args[2])
+
+            if args[0] == 'listdir':
+                response = os.listdir(args[1])
+
+            if args[0] == 'lseekread':
+                os.lseek(args[1], args[2], 0)
+                response = os.read(args[1], args[3])
+
+            if args[0] == 'lseekwrite':
+                os.lseek(args[1], args[2], 0)
+                response = os.write(args[1], args[3])
+
+            if args[0] == 'lstat':
+                response = os.lstat(args[1])
+
+            if args[0] == 'mkdir':
+                os.mkdir(args[1], args[2])
+
+            if args[0] == 'mknod':
+                os.mknod(args[1], args[2], args[3])
+
+            if args[0] == 'open':
+                os.open(args[1], args[2])
+
+            if args[0] == 'readlink':
+                response = os.readlink(args[1])
+
+            if args[0] == 'rename':
+                response = os.rename(args[1], args[2])
+
+            if args[0] == 'rmdir':
+                response = os.rmdir(args[1])
+
+            if args[0] == 'statvfs':
+                response = os.statvfs(args[1])
+
+            if args[0] == 'symlink':
+                response = os.symlink(args[1], args[2])
+
+            if args[0] == 'unlink':
+                response = os.unlink(args[1])
+
+        except OSError as exc:
+            response = exc
+
+        if response is None:
+            return json.dumps(('non', response))
+        if isinstance(response, Exception):
+            return json.dumps(('err', response.errno))
+        else:
+            return json.dumps(('res', response))
+
+    def handle_remote_request(self, s):
+        try:
+            #s is a network client connection
+            data, self.network_client_address = s.recvfrom(512)
+            obj = json.loads(data)
+            if obj[2] == 'ack':
+                log.debug('ack %s' % repr(obj))
+                #find out key info
+                candidate_list = [ctr for ctr in order_of_keys_in_chunk_queue if ctr[1] == obj[1][0] and ctr[3] == obj[1][2]]
+                #remove from chunk_queue
+                key = candidate_list[0]
+                self.order_of_keys_in_chunk_queue.remove(key)
+                del self.chunk_queue[key]
+                self.packets_in_flight -= 1
+            if obj[2] == 'pac' and obj[0][0] not in completed_tasks:
+                log.debug('pac %s' % repr(obj))
+                #add to receive chunk queue queue
+                key = self.augment_timestamp_info_key(obj[0])
+                val = obj[1]
+                #add packet to receive chunk
+                self.receive_chunk_queue[key] = val
+                #send ack
+                s.sendto(json.dumps([0, obj[0], 'ack']), network_client_address)
+                #check if all packets have been received for the same taskid
+                #there's a more efficient way to do this
+                list_of_recv_chunks = [ctr for ctr in receive_chunk_queue.keys() if ctr[0] == key[0]]
+                if len(list_of_recv_chunks) == key[3]:
+                    list_of_recv_chunks.sort(key = lambda x: x[2])
+                    #all chunks have been received
+                    #transfer to receive_queue
+                    self.receive_queue[key[0]] = ''.join([self.receive_chunk_queue.pop(ctr) for ctr in list_of_recv_chunks])
+                    #mark timestamp in completed queue
+                    self.completed_tasks[key[0]] = time.time()
+
+                    #execute action
+                    string_response = execute_message(self.receive_queue.pop(key[0]))
+
+                    #now send response
+                    taskid += 1
+                    #add message to the chunk_queue
+                    (keys, chunks) = split_task(taskid, key[0], string_response)
+                    #add keys to order_of_keys_in_chunk_queue
+                    self.order_of_keys_in_chunk_queue.extend(keys)
+                    #sort by priority
+                    self.order_of_keys_in_chunk_queue.sort(key = lambda x: x[0])
+                    #add entries to chunk_queue
+                    for (key, val) in zip(keys, chunks):
+                        self.chunk_queue[key] = val
+        except:
+            pass                    
+
+    def send_remote_response(self, s):
+        if self.packets_in_flight < 1 and len(self.order_of_keys_in_chunk_queue)>0:
+            #send the topmost priority packet if number of packets in flight is <1
+            #introduce some packet timeout mechanisms
+            key = self.order_of_keys_in_chunk_queue[0]
+            s.sendto(json.dumps([self.remove_priority_timestamp_info_from_key(key), self.chunk_queue[key], 'pac']), self.network_client_address)
+            self.packets_in_flight += 1
+
+
+    def split_task(self, taskid, original_taskid, taskstring):
+        #this splits up the taskstring into a list of chunks
+        startpt = range(0, len(taskstring), 100)
+        chunks = [taskstring[pt:pt + 100] for pt in startpt[:-1]]
+        chunks.append(taskstring[startpt[-1]:len(taskstring)])
+        #smaller the task higher the priority
+        ids = [(len(taskstring), taskid, original_taskid, ctr, len(chunks), time.time()) for ctr in range(len(chunks))]
+        return (ids, chunks)
+
+    def remove_priority_timestamp_info_from_key(self, key):
+        return (key[1], key[2], key[3], key[4])
+
+    def augment_timestamp_info_key(self, key):
+        return (key[0], key[1], key[2], key[3], time.time())
+
+    def main_loop(self):
+        while self.inputs:
+            readable, writable, exceptional = select.select(self.inputs, self.outputs, self.inputs)
+            for s in readable:
+                self.handle_remote_request(s)
+            for s in writable:
+                self.send_remote_response(s)
+
+            for s in exceptional:
+                self.inputs.remove(s)
+                if s in self.outputs:
+                    self.outputs.remove(s)
+
+if __name__=='__main__':
+    network_server = Networkserver()
+    network_server.main_loop()
