6d0e8e4c664165bf46e2d293b393935e739d7dc3
Mainak Chowdhury <mainakch@gmail.com>
2015-02-24 08:28:10 -0800

    refactored  code

--- network_client.py
+++ network_client.py
@@ -1,3 +1,7 @@
+'''
+network_client.py - Sends remote requests, receives response and returns response
+'''
+#TODO: add error checking
 import socket
 import json
 import sys
@@ -5,28 +9,28 @@ import select
 import time
 from random import randint
 
+log = logging.getLogger('network_client')
+log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+log.addHandler(ch)
+
 #socket address
 network_server_address = ('corn29.stanford.edu', 60002)
 client_address = '/tmp/socket_c_and_nc'
 
 #list of sockets
-#socks = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM), socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)]
 unix_server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 network_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #initialize the sockets
-
-#listen to client processes
-#socks[1].bind(client_address)
-#socks[1].listen(1)
 unix_server.bind(client_address)
 unix_server.listen(1)
 
-#inputs = [socks[0], socks[1]]
 inputs = [unix_server, network_server]
 
 outputs = [network_server]
-#outputs = []
 
 #queues
 
@@ -47,7 +51,7 @@ timestamp_transmission = 0
 sock_to_taskid = {}
 taskid_to_sock = {}
 order_of_keys_in_chunk_queue = []
-taskid = randint(0, 1002039)
+taskid = randint(0, 1002039) #not randomizing can lead to same taskid in case of client failure
 packets_in_flight = 0
 
 def split_task(taskid, taskstring):
@@ -64,95 +68,111 @@ def remove_priority_timestamp_info_from_key(key):
 
 def augment_timestamp_info_key(key):
     return (key[0], key[1], key[2], key[3], time.time())
+
+def add_filesystem_request_to_transmit_queue(message, s):
+    taskid += 1
+    #add message to the chunk_queue
+    (keys, chunks) = split_task(taskid, message)
+    #add keys to order_of_keys_in_chunk_queue
+    order_of_keys_in_chunk_queue.extend(keys)
+    #sort by priority
+    order_of_keys_in_chunk_queue.sort(key = lambda x: x[0])
+    #add entries to chunk_queue
+    for (key, val) in zip(keys, chunks):
+        chunk_queue[key] = val
+    #add association between sock and taskid
+    sock_to_taskid[s] = taskid
+    taskid_to_sock[taskid] = s
+    #remove from readables
+    inputs.remove(s)
+    #add to writables
+    outputs.append(s)
+
+    return s
+
+def receive_filesystem_request(s):
+    data = s.recv(10)
+    if data:
+        return s.recv(int(data)) #change this to handle large requests
+
+def handle_remote_filesystem_response(obj):
+    data, network_server_address = s.recvfrom(512)
+    obj = json.loads(data)
+    if obj[2] == 'ack':
+        #find out key info
+        candidate_list = [ctr for ctr in order_of_keys_in_chunk_queue if ctr[1] == obj[1][0] and ctr[3] == obj[1][2]]
+        #remove from chunk_queue
+        key = candidate_list[0]
+        order_of_keys_in_chunk_queue.remove(key)
+        del chunk_queue[key]
+        packets_in_flight -= 1
+    if obj[2] == 'pac' and obj[0][1] not in completed_tasks:
+        #add to receive chunk queue queue
+        key = augment_timestamp_info_key(obj[0])
+        val = obj[1]
+        #add packet to receive chunk
+        receive_chunk_queue[key] = val
+        #send ack
+        s.sendto(json.dumps([0, obj[0], 'ack']), network_server_address)
+        #check if all packets have been received for the same original_task_id
+        #there's a more efficient way to do this
+        list_of_recv_chunks = [ctr for ctr in receive_chunk_queue.keys() if ctr[1] == key[1]]
+        if len(list_of_recv_chunks) == key[3]:
+            list_of_recv_chunks.sort(key = lambda x: x[2])
+            #all chunks have been received
+            #transfer to receive_queue
+            receive_queue[key[1]] = ''.join([receive_chunk_queue.pop(ctr) for ctr in list_of_recv_chunks])
+            #mark timestamp in completed queue
+            completed_tasks[key[1]] = time.time()
+
+def send_packets_to_remote_filesystem(s):
+    #if possible send packets
+    #TODO: add a retransmission timeout
+    if packets_in_flight < 1 and len(order_of_keys_in_chunk_queue)>0:
+        key = order_of_keys_in_chunk_queue[0]
+        s.sendto(json.dumps([remove_priority_timestamp_info_from_key(key), chunk_queue[key], 'pac']), network_server_address)
+        packets_in_flight += 1
+
+def send_response_to_local_filesystem(s):
+    #TODO: add code to handle network error
+    if sock_to_taskid[s] in receive_queue:
+        log.debug('Sending response to filesystem')
+        s.sendall(str(len(receive_queue[sock_to_taskid[s]])).zfill(10))
+        s.sendall(receive_queue[sock_to_taskid[s]])
+
+        #cleanup
+        del taskid_to_sock[sock_to_taskid[s]]
+        del receive_queue[sock_to_taskid[s]]
+        del sock_to_taskid[s]
+        outputs.remove(s)
+
     
 while inputs:
     readable, writable, exceptional = select.select(inputs, outputs, inputs)
     for s in readable:
         if s is unix_server:
-            print 'Inside 1'
+            log.debug('Accept filesystem connection')
             connection, client_address = unix_server.accept()
             connection.setblocking(0)
             inputs.append(connection)
         if s is not unix_server and s.family == socket.AF_UNIX:
-            print 'Inside 2'
+            log.debug('Listen to filesystem connection')
             #s is a client connection from a local process
             #read length of tcp message
-            data = s.recv(10)
-            if data:
-                length = int(data)
-                message = s.recv(length) #maybe change this to read a bounded number of bits at a time
-
-                taskid += 1
-                #add message to the chunk_queue
-                (keys, chunks) = split_task(taskid, message)
-                #add keys to order_of_keys_in_chunk_queue
-                order_of_keys_in_chunk_queue.extend(keys)
-                #sort by priority
-                order_of_keys_in_chunk_queue.sort(key = lambda x: x[0])
-                #add entries to chunk_queue
-                for (key, val) in zip(keys, chunks):
-                    chunk_queue[key] = val
-                #add association between sock and taskid
-                sock_to_taskid[s] = taskid
-                taskid_to_sock[taskid] = s
-                #remove from input
-                inputs.remove(s)
-                #add to output
-                outputs.append(s)
-
+            message = receive_filesystem_request(s)
+            add_filesystem_request_to_transmit_queue(message, s)
         if s.family == socket.AF_INET:
-            print 'Inside network'
+            log.debug('Receiving information from remote filesystem')
             #s is a network client
-            data, network_server_address = s.recvfrom(512)
-            obj = json.loads(data)
-            if obj[2] == 'ack':
-                #find out key info
-                print obj
-                print order_of_keys_in_chunk_queue
-                candidate_list = [ctr for ctr in order_of_keys_in_chunk_queue if ctr[1] == obj[1][0] and ctr[3] == obj[1][2]]
-                #remove from chunk_queue
-                key = candidate_list[0]
-                order_of_keys_in_chunk_queue.remove(key)
-                del chunk_queue[key]
-                packets_in_flight -= 1
-            if obj[2] == 'pac' and obj[0][1] not in completed_tasks:
-                #add to receive chunk queue queue
-                key = augment_timestamp_info_key(obj[0])
-                val = obj[1]
-                #add packet to receive chunk
-                receive_chunk_queue[key] = val
-                #send ack
-                s.sendto(json.dumps([0, obj[0], 'ack']), network_server_address)
-                #check if all packets have been received for the same original_task_id
-                #there's a more efficient way to do this
-                list_of_recv_chunks = [ctr for ctr in receive_chunk_queue.keys() if ctr[1] == key[1]]
-                if len(list_of_recv_chunks) == key[3]:
-                    list_of_recv_chunks.sort(key = lambda x: x[2])
-                    #all chunks have been received
-                    #transfer to receive_queue
-                    receive_queue[key[1]] = ''.join([receive_chunk_queue.pop(ctr) for ctr in list_of_recv_chunks])
-                    #mark timestamp in completed queue
-                    completed_tasks[key[1]] = time.time()
-        
+
+            handle_remote_filesystem_response(s)
+            
     for s in writable:
         if s is network_server:
-            #send the topmost priority packet if number of packets in flight is <1
-            if packets_in_flight < 1 and len(order_of_keys_in_chunk_queue)>0:
-                key = order_of_keys_in_chunk_queue[0]
-                s.sendto(json.dumps([remove_priority_timestamp_info_from_key(key), chunk_queue[key], 'pac']), network_server_address)
-                packets_in_flight += 1
+            send_packets_to_remote_filesystem(s)
+            
         if s is not unix_server and s.family == socket.AF_UNIX:
-            if sock_to_taskid[s] in receive_queue:
-                print 'Sending'
-                print receive_queue[sock_to_taskid[s]]
-                s.sendall(str(len(receive_queue[sock_to_taskid[s]])).zfill(10))
-                s.sendall(receive_queue[sock_to_taskid[s]])
-
-                #cleanup
-                del taskid_to_sock[sock_to_taskid[s]]
-                del receive_queue[sock_to_taskid[s]]
-                del sock_to_taskid[s]
-                outputs.remove(s)
+            send_response_to_local_filesystem(s)
             
     for s in exceptional:
         inputs.remove(s)
